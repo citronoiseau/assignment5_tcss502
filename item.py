ff713ff8b3b2472d9e0238c72f29cc67158542fb
Asia <134224668+citronoiseau@users.noreply.github.com>
2025-11-29 16:19:00 -0800

    added methods to get item type

--- item.py
+++ item.py
@@ -1,25 +1,45 @@
+import random
 from abc import ABC, abstractmethod
 
 
 class Item(ABC):
+    def __init__(self, type):
+        self._type = type
+
+    def get_type(self):
+        return self._type
+
     @abstractmethod
     def display_char(self):
         pass
 
 
 class HealingPotion(Item):
+    def __init__(self):
+        super().__init__("healing_potion")
+
     def display_char(self):
         return "H"
 
 
 class VisionPotion(Item):
+    def __init__(self):
+        super().__init__("vision_potion")
+
     def display_char(self):
         return "V"
 
 
 class Pit(Item):
+    def __init__(self):
+        super().__init__("pit")
+        self._damage = random.randint(5, 20)
+
     def display_char(self):
         return "X"
+
+    def get_damage(self):
+        return self._damage
 
 
 class ItemFactory:
